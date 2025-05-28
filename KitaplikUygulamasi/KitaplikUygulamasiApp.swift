a1ef28651c42d003b9d01a6f9fb019012c710498
halilkrm <halil128kerem123@gmail.com>
2025-05-28 16:24:13 +0300

    Initial Commit

---
+++ KitaplikUygulamasi/KitaplikUygulamasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitaplikUygulamasiApp.swift
+//  KitaplikUygulamasi
+//
+//  Created by Halil Keremoğlu on 28.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct KitaplikUygulamasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
